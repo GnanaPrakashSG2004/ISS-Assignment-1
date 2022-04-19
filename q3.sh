fa068f38c68fea94505ab836570e423d64c1c0cc
GnanaPrakahSG2004 <gnanaprakashpunnavajhala@gmail.com>
2022-04-19 16:00:49 +0530

    Modified file

--- q3.sh
+++ q3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wc -c < $1
-wc -l < $1
+awk 'END{print NR}' $1
 wc -w < $1
 
 awk '{print NF}' $1 > temp.txt
@@ -17,6 +17,9 @@ cat $1 | tr ' ' '\n' | sort | uniq -c > temp.txt
 awk '{print $2 " " $1}' temp.txt > temp2.txt && rm temp.txt
 while read word freq
 do
-    echo "Word: $word - Count of repetition: $freq"
+    if  [[ $freq -gt 1 ]]
+    then
+        echo "Word: $word - Count of repetition: $freq"
+    fi
 done < temp2.txt
 rm temp2.txt
